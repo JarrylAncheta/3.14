4c5c4b1c0d6c63988d0fd8128d4bc5865f5548c2
JarrylAncheta <55153273+JarrylAncheta@users.noreply.github.com>
2021-11-16 01:17:11 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,65 +1,64 @@
-let array = [];
-let backgroundColor = 225;
+let array=[];
+let backgroundColor =200;
 
-function setup() {
-  createCanvas(600, 600);
-  background(backgroundColor);
-  drawGrid();
+function setup(){
+  createCanvas(windowWidth, windowHeight);
+  // background(backgroundColor);
+  strokeWeight(3);
   noFill();
-}
+  drawGrid();
 
-function draw() {
+}
+function draw(){
   if (mouseIsPressed){
-    // line(mouseX, mouseY, pmouseX, pmouseY);
-    backgroundColor-= 4;
-    background(backgroundColor);
-    beginShape();
-    for(let i = 0; i < array.length; i++){
-    // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
-    curveVertex(array[i][0], array[i][1]);
-    }
-    endShape();
-    array.push([mouseX, mouseY]);
-  } 
+      // background(backgroundColor);
+      backgroundColor -=2;
+      // line(mouseX, mouseY, pmouseX,pmouseY);
+      array.push([mouseX, mouseY]);
+      
+  }
+  
 
 }
-
-function keyTyped() {
-  if (key == 's'){
-    //save image
+function keyTyped(){
+  if (key === 's'){
     saveCanvas('fileName','png');
-  } else if (key == 'd'){
-    //display image
+  }else if (key ==='d'){
     background(200);
     beginShape();
-    for(let i = 0; i < array.length; i++){
-    // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
-    curveVertex(array[i][0], array[i][1]);
-  }
-    endShape();
-  }
-  return false
-}
+    for (let i=0; i< array.length; i++){
+      // line(array[i][0],array[i][1],array[i+1][0],array[i+1][1] )
+      curveVertex(array[i][0], array[i][1])
+    } 
+  endShape();
+//   }else if ( key === 'c'){
+//   clear();
+//   background(200);
 
-function mousePressed() {
-  array = []
-  backgroundColor = 225;
+//   array.length = 0;  
+// }
+  return false;
 }
-
-function drawGrid() {
-  strokeWeight(0);
+}
+function mousePressed(){
+  array = [];
+  backgroundColor = 255;
+}
+function drawGrid(){
   numCells = 20;
-  fillColor = 255;
-  for (let i = 0; i <= width; i += width / numCells){
-    for (let j = 0; j <= height; j += height / numCells){
+  fillColor= 255;
+  strokeWeight(0);
+  for ( let i = 0; i<= width; i+=width/numCells){
+    for ( let j = 0; j<= height; j+= height/numCells){
       if (fillColor === 255){
-        fillColor = 200;
-      } else {
-        fillColor = 255;
+        fillColor = 200
+      } else{
+        fillColor =255;
       }
-      fill(fillColor);
+      fill(fillColor); 
       rect(i, j, width/numCells, height/numCells);
     }
+    
   }
-  strokeWeight(5);
 }
+strokeWeight(5);
